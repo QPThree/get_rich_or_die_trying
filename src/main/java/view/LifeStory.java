5c18c8384d48c800204cdf2402fc1b4be61b533f
Sheila Cartagena <93410083+scartagena23@users.noreply.github.com>
2022-02-15 15:12:39 -0500

    commenting code used for testing

--- src/main/java/view/LifeStory.java
+++ src/main/java/view/LifeStory.java
@@ -337,13 +337,13 @@ public class LifeStory {
     }
 
     //    For testing individual page. This should be exported.
-    public static void main(String[] args) {
-        //Schedule a job for the event-dispatching thread:
-        //creating and showing this application's GUI.
-        javax.swing.SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                render();
-            }
-        });
-    }
+//    public static void main(String[] args) {
+//        //Schedule a job for the event-dispatching thread:
+//        //creating and showing this application's GUI.
+//        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+//            public void run() {
+//                render();
+//            }
+//        });
+//    }
 }
