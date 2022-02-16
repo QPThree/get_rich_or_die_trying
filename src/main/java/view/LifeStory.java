e85e9ffefbf6460e524e8a5019b48d6c45e01b2e
Dans7an <kdanstanmark@gmail.com>
2022-02-16 11:29:57 -0500

    collaborated with Zach to update the game UI theme, AB#873

--- src/main/java/view/LifeStory.java
+++ src/main/java/view/LifeStory.java
@@ -18,7 +18,6 @@ public class LifeStory {
     public static JButton option1 = createJButton("Button 1", 200, 150, false);
     public static JButton option2 = createJButton("Button 2", 200, 150, false);
     public static JButton continueButton;
-    public static JPanel optionsPanel;
     public static JTextArea textArea = new JTextArea(20, 20), playerInfoTextArea = new JTextArea(5,5), sceneInfoTextArea = new JTextArea(20,20), playerAttributes = new JTextArea(5, 20);
     public static JLabel healthLabel = new JLabel(), wealthLabel = new JLabel();
     private static Person player = Game.player;
@@ -31,6 +30,7 @@ public class LifeStory {
             pane.setComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT);
         }
         JButton button;
+        JButton helpButton;
         pane.setLayout(new GridBagLayout());
         pane.setSize(800,500);
         GridBagConstraints c = new GridBagConstraints(); // if you choose to use the same one throughout, remember to reset values.
@@ -39,75 +39,95 @@ public class LifeStory {
             c.fill = GridBagConstraints.HORIZONTAL;
         }
 
-        // instantiating player data for testing
-//        testPlayer();
-
-
-//        button = new JButton("Help");
-//        button.addActionListener(e -> System.out.println("Clicked Help"));
-//        if (shouldWeightX) {
-//            c.weightx = 0.5;
-//        }
-//        c.gridx = 0;
-//        c.gridy = 0;
-//        c.gridwidth = 1;
-//        button.setVisible(true);
-//        pane.add(button, c);
-//
-//        button = new JButton("Load");
-//        button.addActionListener(e -> System.out.println("Clicked Load"));
-//        c.weightx = 0.5;
-//        c.gridx = 1;
-//        c.gridy = 0;
-//        button.setVisible(true);
-//        pane.add(button, c);
-//
-//        button = new JButton("exit");
-//        button.addActionListener(e -> System.out.println("Clicked Exit"));
-//        c.weightx = 0.5;
-//        c.gridx = 2;
-//        c.gridy = 0;
-//        button.setVisible(true);
-//        pane.add(button, c);
-
-
-//        playerInfoTextArea.setBounds(0, 50, 100, 100);
-        playerInfoTextArea.setBackground(Color.gray);
+        playerInfoTextArea.setMargin(new Insets(25,100,25,40));
+        playerInfoTextArea.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createLineBorder(Color.darkGray,2,true),
+                BorderFactory.createEmptyBorder(5, 10, 5, 10)));
         playerInfoTextArea.setVisible(true);
-        playerInfoTextArea.setFont(new Font("Hei", Font.BOLD, 12));
-        playerInfoTextArea.setPreferredSize(new Dimension( 550, 100));
-        playerInfoTextArea.setText("Name: " + player.getName() +
-                "  Age: " + player.getAge()
-                + "  Career Choice: " + player.getCareer() +
-                " NetWorth: " + player.getPrettyNetWorth() +
-                " Health: " + player.getHealthPoints());
+        playerInfoTextArea.setFont(new Font("Hei", Font.BOLD, 16));
+        playerInfoTextArea.setPreferredSize(new Dimension( 800, 100));
+        playerInfoTextArea.setText( "\n" +
+                " | Name: " + player.getName() +
+                " | Age: " + player.getAge() +
+                " | Career Choice: " + player.getCareer() +
+                " | NetWorth: " + player.getPrettyNetWorth() +
+                " | Health: " + player.getHealthPoints() + " |\n\n" +
+                " | Strength: " + player.getStrength() +
+                " | Intellect: " + player.getIntellect() +
+                " | Creativity: " + player.getCreativity() + " | \n"
+        );
         c.gridx = 0;
         c.gridy = 0;
+        c.gridwidth = 3;
         pane.add(playerInfoTextArea, c);
 
 //        playerAttributes.setBounds(0, 50, 100, 100);
-        playerAttributes.setBackground(Color.blue);
-        playerAttributes.setVisible(true);
-        playerAttributes.setFont(new Font("Hei", Font.BOLD, 12));
-        playerAttributes.setPreferredSize(new Dimension(250, 100));
-        playerAttributes.setText("Strength: " + player.getStrength() +
-                " Intellect: " + player.getIntellect() + " Creativity: " + player.getCreativity());
+//        playerAttributes.setBackground(Color.blue);
+//        playerAttributes.setVisible(true);
+//        playerAttributes.setFont(new Font("Hei", Font.BOLD, 12));
+//        playerAttributes.setPreferredSize(new Dimension(250, 100));
+//        playerAttributes.setText("Strength: " + player.getStrength() +
+//                " Intellect: " + player.getIntellect() + " Creativity: " + player.getCreativity());
+//
+//        c.gridx = 1;
+//        c.gridy = 0;
+////        c.gridwidth = 1;
+//        pane.add(playerAttributes, c);
 
+        helpButton = new JButton("Help");
+        helpButton.addActionListener(e -> {
+            String currentSceneText = sceneInfoTextArea.getText();
+            System.out.println("Clicked Help");
+            MainFrame.writeToTextArea(sceneInfoTextArea, MainFrame.instructions);
+            MainFrame.removeAllActionListeners(helpButton);
+            MainFrame.changeButtonText(helpButton, "Exit Help");
+            helpButton.addActionListener(el -> {
+                MainFrame.writeToTextArea(sceneInfoTextArea, currentSceneText);
+                MainFrame.changeButtonText(helpButton, "Help");
+            });
+        });
+        if (shouldWeightX) {
+            c.weightx = 0.5;
+        }
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridx = 0;
+        c.gridy = 1;
+        c.gridwidth = 1;
+        helpButton.setVisible(true);
+        pane.add(helpButton, c);
+
+        button = new JButton("Save");
+        button.addActionListener(e -> System.out.println("Clicked Save"));
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.weightx = 0.5;
         c.gridx = 1;
-        c.gridy = 0;
-        pane.add(playerAttributes, c);
+        c.gridy = 1;
+        button.setVisible(true);
+        pane.add(button, c);
+
+        button = new JButton("Exit");
+        button.addActionListener(e -> System.out.println("Clicked Exit"));
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.weightx = 0.5;
+        c.gridx = 2;
+        c.gridy = 1;
+        button.setVisible(true);
+        pane.add(button, c);
 
 //        sceneInfoTextArea.setBounds(0, 400, 800, 800);
-        sceneInfoTextArea.setBackground(Color.pink);
+        sceneInfoTextArea.setMargin(new Insets(25,100,25,40));
+        sceneInfoTextArea.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createLineBorder(Color.darkGray,2,true),
+                BorderFactory.createEmptyBorder(100, 15, -130, 18)));
         sceneInfoTextArea.setVisible(true);
-        sceneInfoTextArea.setFont(new Font("Hei", Font.BOLD, 16));
+        sceneInfoTextArea.setFont(new Font("Hei", Font.BOLD, 18));
         sceneInfoTextArea.setPreferredSize(new Dimension(400, 200));
         sceneInfoTextArea.setWrapStyleWord(true);
         sceneInfoTextArea.setLineWrap(true);
         sceneInfoTextArea.setText(currentScene.getPrompt());
         c.gridx = 0;
-        c.gridy = 1;
+        c.gridy = 2;
+        c.gridwidth = 2;
         c.gridheight = 2;
+        c.insets = new Insets(2,5,5,2);
         pane.add(sceneInfoTextArea, c);
 
         //creating the options panel & buttons
@@ -119,35 +139,22 @@ public class LifeStory {
 
         setListenersForButtons();
         // Option1 grid
-        c.gridx = 1;
-        c.gridy = 1;
+        c.gridx = 2;
+        c.gridy = 2;
         c.gridheight = 1;
         pane.add(option1, c);
 
         //Option 2 grid
-        c.gridx= 1;
-        c.gridy = 2;
+        c.gridx= 2;
+        c.gridy = 3;
         c.gridheight = 1;
         pane.add(option2, c);
 
         continueButton = createJButton("Continue", 800, 50, false);
         c.gridx = 0;
         c.gridy = 4;
-        c.gridwidth = 2;
+        c.gridwidth = 4;
         pane.add(continueButton, c);
-
-//        healthLabel.setText("Health: " + player.getHealthPoints());
-//        c.gridx = 0;
-//        c.gridy = 4;
-//        c.gridwidth = 1;
-//        pane.add(healthLabel, c);
-//
-//        wealthLabel.setText("Net worth: " + player.getPrettyNetWorth());
-//        c.gridx = 1;
-//        c.gridy = 4;
-//        c.gridwidth = 1;
-//        pane.add(wealthLabel, c);
-
         pane.revalidate();
         pane.setVisible(true);
     }
@@ -166,7 +173,7 @@ public class LifeStory {
     }
 
     private static void writeToTextArea(JTextArea textArea, Color color, String string) {
-        textArea.setFont(new Font("Arial", Font.BOLD, 16));
+        textArea.setFont(new Font("Hei", Font.BOLD, 18));
         textArea.setBackground(color);
         textArea.setText(string);
         option1.setVisible(false);
@@ -292,11 +299,16 @@ public class LifeStory {
 
     private static void playerUpdateAge() {
         player.addAge(5);
-        playerInfoTextArea.setText("Name: " + player.getName() +
-                "  Age: " + player.getAge()
-                + "  Career Choice: " + player.getCareer() +
-                " NetWorth: " + player.getPrettyNetWorth() +
-                " Health: " + player.getHealthPoints());
+        playerInfoTextArea.setText("\n" +
+                " | Name: " + player.getName() +
+                " | Age: " + player.getAge() +
+                " | Career Choice: " + player.getCareer() +
+                " | NetWorth: " + player.getPrettyNetWorth() +
+                " | Health: " + player.getHealthPoints() + "\n\n" +
+                " | Strength: " + player.getStrength() +
+                " | Intellect: " + player.getIntellect() +
+                " | Creativity: " + player.getCreativity() + " | \n"
+        );
         playerInfoTextArea.updateUI();
         System.out.println("updated age");
     }
@@ -337,13 +349,13 @@ public class LifeStory {
     }
 
     //    For testing individual page. This should be exported.
-//    public static void main(String[] args) {
-//        //Schedule a job for the event-dispatching thread:
-//        //creating and showing this application's GUI.
-//        javax.swing.SwingUtilities.invokeLater(new Runnable() {
-//            public void run() {
-//                render();
-//            }
-//        });
-//    }
+    public static void main(String[] args) {
+        //Schedule a job for the event-dispatching thread:
+        //creating and showing this application's GUI.
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                render();
+            }
+        });
+    }
 }
