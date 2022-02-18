8b3046dbd512ea891784bf8ef6fad8063d51d3e6
Sheila Cartagena <93410083+scartagena23@users.noreply.github.com>
2022-02-18 17:34:39 -0500

    fixed text it does not displa null when your choice does not impact the money

--- src/main/java/controller/Game.java
+++ src/main/java/controller/Game.java
@@ -302,7 +302,7 @@ public class Game {
         System.out.println(salaryBreakdown);
         // This is currently being used to output the summary.
         // This can go away when serialization is implemented
-        values += (player.getMoneyChange() == null ? "No didn't gain or lose money from your decision" :player.getMoneyChange() +"\n");
+        values += (player.getMoneyChange() == null ? "Did not gain or lose money from your decision" :player.getMoneyChange() +"\n");
         values += (player.getHealthChange() == null ? "No changes to health from your decision" : player.getHealthChange() +"\n\n");
         values += ("++++++ 5-Year Summary ++++++");
         values += ("\nPlayer: " + player.getName());
